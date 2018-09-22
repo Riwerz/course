95031582ddfaf3e30fa37029de2d6b63b6e63ec5
EgorPopruga <najibator228@gmail.com>
2018-09-22 10:41:11 +0300

    Added login panel

--- rootApp/views.py
+++ rootApp/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 # Create your views here.
 def load_mainpage(request):
-    return render(request, 'mainpage.html')
+    return render(request, 'wrapper.html')
 
 def load_login(request):
-    return render(request, 'mainpage.html')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return render(request, 'wrapper.html')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
